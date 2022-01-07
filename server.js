2cc29f894c0a3a04773de92a833ce52af698616e
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-01-07 23:15:29 +0530

    cors Middleware Added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Middleware
+app.use(cors());
 
 // connect to the database
 const DB_URL = process.env.DB_URL;
@@ -15,28 +19,6 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Connected to the database"));
 
 app.use(express.json());
-
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 
 // Routes
 const notesRouter = require("./routes/notes");
