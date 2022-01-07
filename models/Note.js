9c6debbf343a2a50a2ff56fd5362dd9c1f42ef27
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-01-07 19:22:54 +0530

    Basic app done

---
+++ models/Note.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Note", NoteSchema);
---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// connect to the database
+const DB_URL = process.env.DB_URL;
+mongoose.connect(
+    DB_URL,
+    { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to the database"));
+
+app.use(express.json());
+
+// Routes
+const notesRouter = require("./routes/notes");
+app.use("/notes", notesRouter);
+
+// Send json respond to the client
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`)
+});
