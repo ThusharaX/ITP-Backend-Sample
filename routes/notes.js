a605c40de2805568df251602e9df23a6b4896ce4
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-01-07 19:37:24 +0530

    Error fixed

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Note = require("../models/note");
+const Note = require("../models/Note");
 
 // Get all notes
 router.get("/", async (req, res) => {
