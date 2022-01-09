60d34e24af75ead7af4ac46ab08a866e04f60baa
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-01-10 01:18:50 +0530

    Note Schema required: true removed

--- src/models/Note.js
+++ src/models/Note.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const NoteSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
     },
     content: {
         type: String,
-        required: true
     },
     date: {
         type: Date,
