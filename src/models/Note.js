1d53c7862366835fb0295546bb72e2e415115461
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-01-10 01:21:36 +0530

    Revert "Note Schema required: true removed"
    This reverts commit 60d34e24af75ead7af4ac46ab08a866e04f60baa.

--- src/models/Note.js
+++ src/models/Note.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const NoteSchema = new mongoose.Schema({
     title: {
         type: String,
+        required: true
     },
     content: {
         type: String,
+        required: true
     },
     date: {
         type: Date,
